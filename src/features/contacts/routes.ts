83ee34e26067baa9f6f5b5ee09f13736b534a868
Yam C Borodetsky <yam@yam.codes>
2026-01-30 00:49:41 +0500

    Refactor contact routes to use updated etaRenderer and simplify template path

--- src/features/contacts/routes.ts
+++ src/features/contacts/routes.ts
@@ -1,13 +1,16 @@
 import { Hono } from "hono";
 import * as Contact from "./model";
+import { etaRenderer } from "@/core/template";
 
 const contacts = new Hono();
+
+contacts.use(etaRenderer(`${import.meta.dir}/views`));
 
 // /contacts with optional search
 contacts.get("/", (c) => {
 	const search = c.req.query("q");
 	const contactList = search ? Contact.search(search) : Contact.all();
-	return c.render("contacts/views/index.eta", { contacts: contactList, search });
+	return c.render("index.eta", { contacts: contactList, search });
 });
 
 export default contacts;
