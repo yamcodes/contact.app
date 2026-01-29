1a7aede057b1681133261eeb1fa3f2dd53f8a45f
Yam C Borodetsky <yam@yam.codes>
2026-01-30 01:43:42 +0500

    Add static file serving and include styles in layout

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { eta } from "./middleware/eta";
 import router from "./router";
 import { setupHmr } from "./utils/setup-hmr";
 
 const app = new Hono();
 
+app.use("/static/*", serveStatic({ root: "./" }));
 app.use(eta());
 setupHmr(app);
 
