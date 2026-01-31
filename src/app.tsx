9e5711afb000d1b88d5ddf65bc1c61a43b9a2b97
Yam Borodetsky <yam@yam.codes>
2026-01-31 18:47:28 +0500

    Add JSX middleware for custom rendering; refactor app to use new middleware

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,13 @@
 import { Hono } from "hono";
-import { jsxRenderer } from "hono/jsx-renderer";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { StatusCodes } from "http-status-codes";
-import { Layout, NotFound } from "./components";
+import { NotFound } from "./components";
 import { flash } from "./middleware/flash";
 import { htmx } from "./middleware/htmx";
+import { jsx } from "./middleware/jsx";
 import router from "./router";
 import { setupHmr } from "./utils/hmr";
 import { setupStatic } from "./utils/static";
-
-declare module "hono" {
-	type ContextRenderer = (
-		content: string | Promise<string>,
-		props?: { title?: string },
-	) => Response | Promise<Response>;
-}
 
 const app = new Hono();
 
@@ -22,20 +15,8 @@ app.use(trimTrailingSlash());
 setupStatic(app);
 app.use(flash());
 app.use(htmx());
+app.get("*", jsx());
 setupHmr(app);
-
-app.get(
-	"*",
-	jsxRenderer(
-		({ children, title }, c) => {
-			if (c.get("htmx")) {
-				return <>{children}</>;
-			}
-			return Layout({ title, flash: c.get("flash"), children });
-		},
-		{ docType: false },
-	),
-);
 
 app.route("/", router);
 
