e978df0d5737da869d8613c906f4bbee54078885
Yam Borodetsky <yam@yam.codes>
2026-03-13 20:35:45 +0500

    Add `Contact` entity, update `ContactController`, and create `contacts` list template
    - Define `Contact` entity with JPA annotations.
    - Update `ContactController` to support rendering the `contacts` page with a placeholder list.
    - Add Thymeleaf `contacts/list.html` template for listing contacts.
    - Create reusable `layout.html` template.

---
+++ src/main/java/codes/yam/contacts/Contact.java
@@ -0,0 +1,21 @@
+package codes.yam.contacts;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Contact {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String slug;
+  private String first;
+  private String last;
+  private String email;
+  private String phone;
+}
--- src/main/java/codes/yam/contacts/ContactController.java
+++ src/main/java/codes/yam/contacts/ContactController.java
@@ -1,17 +1,20 @@
 package codes.yam.contacts;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/contacts";
-    }
+  @GetMapping("/")
+  public String index() {
+    return "redirect:/contacts";
+  }
 
-    @GetMapping("/contacts")
-    public String contacts() {
-        return "contacts";
-    }
+  @GetMapping("/contacts")
+  public String contacts(Model model) {
+    model.addAttribute("contacts", List.of());
+    return "contacts/list";
+  }
 }
