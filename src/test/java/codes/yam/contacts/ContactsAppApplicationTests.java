b10a1fdf98b67935c061b670cfa49cdfb8c1658c
Yam Borodetsky <yam@yam.codes>
2026-03-13 13:22:45 +0500

    Initialize Contacts App project with Spring Boot and Maven configuration
    - Add Maven Wrapper for dependency management.
    - Set up Spring Boot application structure with necessary files: `ContactsAppApplication` and `application.yaml`.
    - Configure `pom.xml` with required dependencies for Spring Boot, JPA, Thymeleaf, and testing.
    - Add `.gitignore

---
+++ src/test/java/codes/yam/contacts/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.yam.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
