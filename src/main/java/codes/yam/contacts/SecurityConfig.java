2225bed96431b8bd4bc6907b0da71e172e263123
Yam C Borodetsky <yam@yam.codes>
2026-03-20 16:53:32 +0500

    Add Spring Security configuration and enable H2 console in dev mode (#40)
    * Add Spring Security configuration and enable H2 console in dev mode
    
    - Introduce `SecurityConfig` with authentication and form login setup.
    - Add `spring-boot-starter-security` dependency to `pom.xml`.
    - Enable H2 console for development in `application-dev.yaml`.
    
    * [autofix.ci] apply automated fixes
    
    * Add Spring Security configuration and enable H2 console in dev mode
    
    - Introduce `SecurityConfig` with authentication and form login setup.
    - Add `spring-boot-starter-security` dependency to `pom.xml`.
    - Enable H2 console for development in `application-dev.yaml`.
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ src/main/java/codes/yam/contacts/SecurityConfig.java
@@ -0,0 +1,24 @@
+package codes.yam.contacts;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    return http.authorizeHttpRequests(
+            auth -> auth.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+        .build();
+  }
+}
